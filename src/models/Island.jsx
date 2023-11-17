9b5c13611374e081bfbaab241c2796297267a354
Mateo071 <floresmateo2000@gmail.com>
2023-11-17 18:43:06 -0500

    13003

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -8,7 +8,7 @@ Title: Fox's islands
 */
 
 import React, { useRef, useEffect } from "react";
-import { useGLTF } from "@react-three/drei";
+import { MeshPortalMaterial, useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from '@react-three/fiber'
 import { a } from '@react-spring/three'
 
@@ -103,6 +103,39 @@ export const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props })
       document.removeEventListener('keyup', handleKeyUp);
     }
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
+    } else {
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
 
   return (
     <a.group ref={islandRef} {...props}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,12 @@ import Island from '../models/Island'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
+import HomeInfo from '../components/HomeInfo'
 
-      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        POPUP
-      </div> */}
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
-  const [currentState, setCurrentStage] = useState(1);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -47,6 +45,9 @@ const Home = () => {
 
   return (
     <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        {currentStage === 1 && <HomeInfo currentStage={currentStage} />}
+      </div>
       <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`} camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -54,7 +55,7 @@ const Home = () => {
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={isRotating} setIsRotating={setIsRotating} setCurrentState={setCurrentStage} />
           <Plane isRotating={isRotating} planeScale={planeScale} planePosition={planePosition} rotation={[0, 20, 0]} />
         </Suspense>
